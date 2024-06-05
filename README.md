95f94da0de5783f131e2db7ce4dff01ea5a75ee5
Mohtashim <shoaibmohtashim973@gmail.com>
2024-06-05 19:40:14 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Printrov Merch Store
+
+Printrov Merch Store built on frappe
+
+#### License
+
+MIT
---
+++ printrov_merch_store/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Printrov Merch Store",
+			"type": "module",
+			"label": _("Printrov Merch Store")
+		}
+	]
